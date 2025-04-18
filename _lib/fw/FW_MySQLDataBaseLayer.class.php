4be0c69b9da6c68f30b4b3262979eb12391ca807
Clemens <clemens@example.com>
2025-04-18 20:39:31 +0200

    license-info

--- _lib/fw/FW_MySQLDataBaseLayer.class.php
+++ _lib/fw/FW_MySQLDataBaseLayer.class.php
@@ -1,15 +1,31 @@
 <?php
+/*
+ File: FW_MySQLDataBaseLayer.class.php
+ Copyright (c) 2014 Clemens K. (https://github.com/metacreature)
+ 
+ MIT License
+ 
+ Permission is hereby granted, free of charge, to any person obtaining a copy
+ of this software and associated documentation files (the "Software"), to deal
+ in the Software without restriction, including without limitation the rights
+ to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+ copies of the Software, and to permit persons to whom the Software is
+ furnished to do so, subject to the following conditions:
+ 
+ The above copyright notice and this permission notice shall be included in all
+ copies or substantial portions of the Software.
+ 
+ THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+ IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+ FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+ AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+ LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+ OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
+ SOFTWARE.
+*/
 
-/**
- * FW_MySQLDataBaseLayer
- * - handles the database-conection
- * - provides many standard-features and actions like showTables() or getAssocResults()
- * - automatic unlock tables before shutdown
- * - can print and save querylogs depending on the settings even automatic
- * @author Clemens Köhler
- * @copyright Copyright (c) 2010, Clemens Köhler
- * @todo dumpDBData not started
- */
+
+
 class FW_MySQLDataBaseLayer
 {
 
