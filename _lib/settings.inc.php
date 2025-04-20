72f2149e9e4ffa68c1b2ed0e5b9bab919a1fe872
Clemens <clemens@example.com>
2025-04-20 12:14:01 +0200

    day 9 github improvements

--- _lib/settings.inc.php
+++ _lib/settings.inc.php
@@ -27,14 +27,9 @@
 define('TEST_SERVER', strpos($_SERVER['HTTP_HOST'], 'localhost') !== false);
 
 // settings
-define('SETTINGS_LANDING_PAGE', '/recipes/list');
 define('SETTINGS_ALLOW_REGISTER', true);
 define('SETTINGS_LIST_REQUIRES_LOGIN', false);
 define('SETTINGS_DEFAULT_LANG', 'de');
-/*define('SETTINGS_AVAILABLE_LANG', [
-	'de' => 'Deutsch',
-	//'en' => 'English',
-]);*/
 
 
 // Email-config
@@ -47,45 +42,52 @@ define('EMAIL_GREETING_NAME', 'Metacreature');
 
 // FW-Config
 define('USER_TIMEZONE', 'Europe/Vienna');
-define('DOCUMENT_ROOT', $_SERVER['DOCUMENT_ROOT'].(TEST_SERVER ? '' : '/rezepte'));
-define('GALLERY_ROOT', DOCUMENT_ROOT . '/gallery');
-define('WEB_ROOT', '');
-define('GALLERY_WEB_ROOT', '/gallery');
 define('SECURE_SALT', '18dd27efc08342874wgdfhdhz28265203e964c9');
 define('HIDDEN_IMAGEFOLDER_SECURE', 'kjdfgutalocveabta');
 
 
-
-// config
 if (TEST_SERVER) {
 	
 	// db-config
 	define('DB_HOST', 'localhost');
 	define('DB_USERNAME', 'metacreature');
 	define('DB_PASSWORD', 'jhdfd865#838383');
-	define('DB_NAME', 'rezepte');
+	define('DB_NAME', 'recipes');
 	define('DB_PERSISTENT', false);
 	
+	// error-logging
     define('DEBUG_MODE', true);
     ini_set('error_reporting', E_ALL);
     ini_set('display_errors', true);
-    define('WEB_URL', 'http://rezepte.localhost');
-    define('WEB_DOMAIN', 'rezepte.localhost');
+
+	// domain
+    define('WEB_URL', 'http://recipes.localhost');
+    define('WEB_DOMAIN', 'recipes.localhost');
 } else {
 	
 	// db-config
 	define('DB_HOST', 'localhost');
 	define('DB_USERNAME', 'domain_metacreature');
 	define('DB_PASSWORD', 'hkzuyzhjyd');
-	define('DB_NAME', 'domain_rezepte');
+	define('DB_NAME', 'domain_recipes');
 	define('DB_PERSISTENT', false);
 	
+	// error-logging
     define('DEBUG_MODE', false);
     ini_set('error_reporting', 0);
     ini_set('display_errors', false);
-    define('WEB_URL', 'https://rezepte.metacreature.com');
-    define('WEB_DOMAIN', 'rezepte.metacreature.com');
+
+	// domain
+    define('WEB_URL', 'https://recipes.metacreature.com');
+    define('WEB_DOMAIN', 'recipes.metacreature.com');
 }
+
+// other (don't modify)
+define('SETTINGS_LANDING_PAGE', '/recipes/list');
+define('DOCUMENT_ROOT', $_SERVER['DOCUMENT_ROOT'].(TEST_SERVER ? '' : ''));
+define('GALLERY_ROOT', DOCUMENT_ROOT . '/gallery');
+define('WEB_ROOT', '');
+define('GALLERY_WEB_ROOT', '/gallery');
 
 date_default_timezone_set(USER_TIMEZONE);
 
