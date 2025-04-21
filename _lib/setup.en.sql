178baa05b3bbc64a4850bfe4649cd1e04143dbb4
Clemens <clemens@example.com>
2025-04-21 17:25:12 +0200

    english units & categories

---
+++ _lib/setup.en.sql
@@ -0,0 +1,36 @@
+
+INSERT INTO tbl_category (category_name) VALUES ('cocktail'), ('soup'), ('starter'), ('main course'), ('dessert/cake'), ('snack'), ('sauce'), ('pastry'), ('cookies');
+
+INSERT INTO tbl_unit (unit_name, locked) VALUES
+('lb', 1),
+('oz', 1),
+('kg', 1),
+('dec', 1),
+('g', 1),
+('l', 1),
+('cl', 1),
+('ml', 1),
+('tbsp', 1),
+('tsp', 1),
+('pcs', 1),
+('pinch', 1),
+('bunch', 1),
+('stem', 1),
+('twig', 1),
+('leaf', 1),
+('roll', 1),
+('pkg', 1),
+('tube', 1),
+('cup', 1),
+('bottle', 1);
+
+INSERT INTO tbl_tag (tag_name, locked) VALUES
+('fast', 1),
+('time-consuming', 1),
+('fresh', 1),
+('freezable', 1),
+('cheap', 1),
+('expensive', 1),
+('simple', 1),
+('complex', 1),
+('guests', 1);
